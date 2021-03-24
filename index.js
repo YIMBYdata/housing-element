5bf0fee4879b770b5439559edc3e90b32f2e5d9d
Vincent Woo <me@vincentwoo.com>
2021-03-23 17:32:59 -0700

    port to vue

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
 // global object holding all records, keyed by airtable id
-window.cities = {}
+const cities = {}
 
 const Airtable = require('airtable')
 const base = new Airtable({ apiKey: 'keyZHNl5mF6KiNGzA' }).base(
   'appRD2z3VXs78iq80'
 )
 
+const data = { city: null }
+const app = new Vue({
+  el: '#rhna-data-app',
+  data: data,
+  computed: {
+    progressColor: function () {
+      if (this.city.progress < 50) {
+        return 'red'
+      } else if (this.city.progress < 90) {
+        return 'orange'
+      } else if (this.city.progress < 200) {
+        return 'green'
+      } else {
+        return 'green'
+      }
+    },
+  },
+  updated: function () {
+    if (!this.city) return
+    const charts = document.getElementById('rhna-charts')
+    charts.innerHTML = ''
+    const canvas = document.createElement('canvas')
+    canvas.height = 200
+    canvas.width = 400
+    charts.appendChild(canvas)
+
+    const ctx = canvas.getContext('2d')
+
+    const chart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: [
+          'Very Low Income',
+          'Low Income',
+          'Moderate Income',
+          'Above Moderate Income',
+        ],
+        datasets: [
+          {
+            label: '6th Cycle RHNA',
+            backgroundColor: ['#1e697a', '#7a0027', '#db6400', '#f8a62b'],
+            data: [this.city.vli, this.city.li, this.city.mi, this.city.ami],
+          },
+        ],
+      },
+      options: {
+        legend: { display: true },
+        title: {
+          display: true,
+          fontSize: 16,
+          fontColor: '#000',
+          text: '6th Cycle RHNA Allocation: By Income',
+        },
+      },
+    })
+  },
+})
+
 function selectionChanged(id) {
-  const cityData = cities[id]
-  console.log(cityData)
-  populateEasyTextFields(cityData)
-  populateStatementForCity(cityData)
-  populateDueDaysForCity(cityData)
-  populateRHNAPieChartForCity(cityData)
-  populateResourceLinksForCity(cityData)
-  document.getElementById('rhna-data').style.display = 'block'
+  data.city = cities[id]
+  document.getElementById('rhna-data-app').style.display = 'block'
   window.location.hash = id
 }
 
@@ -36,115 +88,6 @@ function populateSelectorWithCities(cities) {
   }
 }
 
-function populateRHNAPieChartForCity(city) {
-  const charts = document.getElementById('rhna-charts')
-  charts.innerHTML = ''
-  const canvas = document.createElement('canvas')
-  canvas.height = 200
-  canvas.width = 400
-  charts.appendChild(canvas)
-
-  const ctx = canvas.getContext('2d')
-
-  const chart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: [
-        'Very Low Income',
-        'Low Income',
-        'Moderate Income',
-        'Above Moderate Income',
-      ],
-      datasets: [
-        {
-          label: '6th Cycle RHNA',
-          backgroundColor: ['#1e697a', '#7a0027', '#db6400', '#f8a62b'],
-          data: [city.vli, city.li, city.mi, city.ami],
-        },
-      ],
-    },
-    options: {
-      legend: { display: true },
-      title: {
-        display: true,
-        fontSize: 16,
-        fontColor: '#000',
-        text: '6th Cycle RHNA Allocation: By Income',
-      },
-    },
-  })
-}
-
-function populateEasyTextFields(city) {
-  for (var field of [
-    'jurisdiction',
-    'county',
-    'council',
-    'population',
-    'density',
-    'countVolunteers',
-  ]) {
-    document.getElementById(field).innerText = city[field] || 'missing'
-  }
-}
-
-function populateDueDaysForCity(city) {
-  today = new Date()
-  const dueDateStr = city.duedate
-  const due = new Date(dueDateStr)
-  const one_day = 1000 * 60 * 60 * 24
-  const days_left = Math.floor((due.getTime() - today.getTime()) / one_day)
-  document.getElementById('element-due-date').innerText = dueDateStr
-  document.getElementById('element-due-days').innerText = days_left
-}
-
-function populateStatementForCity(city) {
-  const statement = document.getElementById('progress-statement')
-  const progress = city.prog
-
-  document.getElementById('5th-count').innerText = city.rhna5
-  document.getElementById('6th-count').innerText = city.rhna6
-
-  if (
-    progress === '' ||
-    progress === 'NaN' ||
-    progress === '0' ||
-    progress === '0%' ||
-    progress === '0.0%'
-  ) {
-    return // No percent completion data.
-  }
-
-  const blame = document.getElementById('progress-blame')
-  const progressBlurb = document.getElementById('progress-blurb')
-  let numericPct = parseFloat(progress)
-  let pct = Math.round(numericPct * 100)
-  if (numericPct < 0.5) {
-    blame.innerText = `${pct}%`
-    blame.style.color = 'red'
-  } else if (numericPct < 0.9) {
-    blame.innerText = `${pct}%`
-    blame.style.color = 'orange'
-  } else if (numericPct < 2.0) {
-    blame.innerText = `${pct}%`
-    blame.style.color = 'green'
-  } else {
-    blame.innerText = `${pct}% - Something is up!`
-    blame.style.color = 'green'
-  }
-  progressBlurb.style.visibility = numericPct > 0.9 ? 'visible' : 'hidden'
-}
-
-function populateResourceLinksForCity(city) {
-  const fifthElemLink = document.getElementById('5th-element')
-  fifthElemLink.href = city.fifthElement
-  fifthElemLink.innerText = city.fifthElement
-
-  const sixthElemDraftLink = document.getElementById('6th-element')
-  sixthElemDraftLink.href = city.elemDraft
-  sixthElemDraftLink.innerText = city.elemDraft
-}
-
 function normalizeRecord(record) {
   const fields = {
     ami: 'AMI',
@@ -153,7 +96,7 @@ function normalizeRecord(record) {
     countVolunteers: 'Count (Interested Volunteers)',
     county: 'County_display',
     density: 'Density',
-    duedate: 'Due Date',
+    dueDate: 'Due Date',
     elemDraft: 'Housing Element Draft',
     fifthElement: '5th Cycle Housing Element',
     finalAllocation: 'Final Allocation',
@@ -161,7 +104,7 @@ function normalizeRecord(record) {
     li: 'LI',
     mi: 'MI',
     population: 'Population',
-    prog: '5th Cycle Progress %',
+    progress: '5th Cycle Progress %',
     rhna5: '5th Cycle RHNA (Total)',
     rhna6: 'Total 6th c. RHNA (current)',
     vli: 'VLI',
@@ -174,6 +117,29 @@ function normalizeRecord(record) {
     }
     fields[key] = value
   }
+
+  if (fields.dueDate) {
+    const today = new Date()
+    const one_day = 1000 * 60 * 60 * 24
+    fields.dueDate = new Date(fields.dueDate)
+    fields.dueDaysLeft = Math.floor(
+      (fields.dueDate.getTime() - today.getTime()) / one_day
+    )
+  }
+
+  switch (fields.progress) {
+    case '':
+    case 'NaN':
+    case '0':
+    case '0%':
+    case '0.0%':
+    case undefined:
+      fields.progress = null
+      break
+    default:
+      fields.progress = Math.round(parseFloat(fields.progress) * 100)
+  }
+
   fields.id = record.id
   return fields
 }
